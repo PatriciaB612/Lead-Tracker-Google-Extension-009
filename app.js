d76d98bba2f9e490d177800df3c5183385ede5fd
Silvia Patricia Soto <122170353+PatriciaB612@users.noreply.github.com>
2024-10-24 17:46:47 -0400

    Changing app from leads tracker to a reads tracker

---
+++ app.js
@@ -0,0 +1,49 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js'
+
+const firebaseConfig = {}
+
+const app = initializeApp(firebaseConfig)
+
+let reads = []
+const inputEl = document.getElementById('input-el')
+const inputBtn = document.getElementById('input-btn')
+const ulEl = document.getElementById('ul-el')
+const readsFromLocalStorage = JSON.parse(localStorage.getItem('reads'))
+const deleteBtn = document.getElementById('delete-btn')
+const tabBtn = document.getElementById('tab-btn')
+
+console.log(app)
+
+if (readsFromLocalStorage) {
+  reads = readsFromLocalStorage
+  render(reads)
+}
+
+tabBtn.addEventListener('click', function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    reads.push(tabs[0].url)
+    localStorage.setItem('reads', JSON.stringify(reads))
+    render(reads)
+  })
+})
+
+inputBtn.addEventListener('click', function () {
+  reads.push(inputEl.value)
+  inputEl.value = ''
+  localStorage.setItem('reads', JSON.stringify(reads))
+  render(reads)
+})
+
+function render(arr) {
+  let arrItems = ''
+  for (let i = 0; i < arr.length; i++) {
+    arrItems += `<li><a href="${arr[i]}" target="_blank">${arr[i]}</a></li>`
+  }
+  ulEl.innerHTML = arrItems
+}
+
+deleteBtn.addEventListener('click', function () {
+  reads = []
+  localStorage.clear()
+  render(reads)
+})
